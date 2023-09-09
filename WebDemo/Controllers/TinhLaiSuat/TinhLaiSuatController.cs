6582cae194e4ee2a6d1adab999906047c246bd60
hungminh0907 <hungminh0907@gmail.com>
2023-09-09 18:05:30 +0700

    Add project files.

---
+++ WebDemo/Controllers/TinhLaiSuat/TinhLaiSuatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDemo.Models.TinhLaiSuat;
+
+namespace WebDemo.Controllers.NewFolder
+{
+    public class TinhLaiSuatController : Controller
+    {
+        public IActionResult TinhLaiSuat()
+        {
+            var model = new ModelTinhLaiSuat();
+            ViewData["Message"] = "";
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult TinhLaiSuat(ModelTinhLaiSuat input)
+        {
+           
+            
+                var model = new ModelTinhLaiSuat();
+                var rexin = new ModelTinhLaiSuat(input.soTien, input.laiSuat, input.kyHan);
+                ViewData["Message"] = "";
+                return View(rexin);
+           
+            
+        }
+    }
+}
---
+++ WebDemo/Models/TinhLaiSuat/ModelTinhLaiSuat.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebDemo.Models.TinhLaiSuat
+{
+    public class ModelTinhLaiSuat
+    {
+        //Khởi tạo hàm mặc định
+        public ModelTinhLaiSuat() { }
+        public int soTien { get; set; }
+
+        public int laiSuat { get; set; }
+
+        public int kyHan { get; set; }
+
+        public int Total { get; set; }
+
+        public int Sum { get; set; }
+
+        // khởi tạo hàm có tham số 
+        public ModelTinhLaiSuat(int soTien, int laiSuat, int kyHan)
+        {
+            this.soTien = soTien;
+            this.laiSuat = laiSuat;
+            this.kyHan = kyHan;
+            this.Total = soTien * laiSuat / 100 * kyHan / 360;
+            this.Sum = soTien + Total;
+        }
+    }
+}
